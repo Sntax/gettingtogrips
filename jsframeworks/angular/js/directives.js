7efb85bac72f19c7725108b6a8827018dc499f1c
Sntax <andrewjrhill@gmail.com>
2015-08-07 20:36:51 +0200

    Angular: Adding selection submenu, sorting results from multi-reddit, fixing links

---
+++ jsframeworks/angular/js/directives.js
@@ -0,0 +1,12 @@
+/* ===================================================== */
+/* Author:                                   Andrew Hill */
+/* Component:                                     Devpad */
+/* Email:                         andrewjrhill@gmail.com */
+/* Project URL:                                      TBA */
+/* Description:                   Application Directives */
+/* ===================================================== */
+
+// Setup application controllers to manage scope on page
+(function () {
+  'use strict';
+}());
